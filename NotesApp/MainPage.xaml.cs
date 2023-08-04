484c673567057f26efdf1952b6e62aba22467e50
Boris Sea <seaborisrc@gmail.com>
2023-08-03 20:34:56 -0400

    First steps to Notes App

---
+++ NotesApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace NotesApp;
+
+public partial class MainPage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public MainPage()
+	{
+		InitializeComponent();
+
+        if (File.Exists(_fileName))
+        {
+            editor.Text = File.ReadAllText(_fileName);
+        }
+    }
+
+    void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
